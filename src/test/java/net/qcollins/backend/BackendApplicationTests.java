9baafa172c89c9085af21cc2d7d7594dbcfddef0
Quinn Collins qxc29 <qcollins@cas.org>
2019-01-02 17:32:56 -0500

    Test

--- src/test/java/net/qcollins/backend/BackendApplicationTests.java
+++ src/test/java/net/qcollins/backend/BackendApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BackendApplicationTests {
-
+// Test
 	@Test
 	public void contextLoads() {
 	}
